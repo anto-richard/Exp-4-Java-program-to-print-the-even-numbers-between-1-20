42d7d95c3648ee6eb446f9bcc0387653b628d7d0
Anto Richard. S <93427534+anto-richard@users.noreply.github.com>
2023-05-14 19:59:12 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,15 @@
+// Java program to print the even numbers from 1 to 20....
+
+public class EvenNumbers
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+"\t");
+            }
+        }
+    }
+}
